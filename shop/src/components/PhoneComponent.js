b7aa2cdd717609bae947c60c8a4ab05753ae4ad1
rusmanov <usmanov.riyaz22@gmail.com>
2023-10-27 11:09:37 +0200

    redux in phoneComponent

--- shop/src/components/PhoneComponent.js
+++ shop/src/components/PhoneComponent.js
@@ -1,15 +1,32 @@
 import React from "react";
 import phone from "../images/phone.png";
+import { connect } from "react-redux";
+import { buyPhone } from "../redux/phone/actionPhone";
 
-export default function PhoneComponent() {
+function PhoneComponent(props) {
+  console.log(props);
   return (
     <div className="container">
       <img src={phone} alt="phone"></img>
       <p>
         Disponibilite:
-        <span id="count"></span>
+        <span id="count">{props.phones}</span>
       </p>
-      <button>Acheter</button>
+      <button onClick={() => props.buyPhone()}>Acheter</button>
     </div>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    phones: state.phones,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyPhone: () => dispatch(buyPhone()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PhoneComponent);
