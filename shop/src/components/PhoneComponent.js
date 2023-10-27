132836909139a213b4b15a4683f685288a489bb8
rusmanov <usmanov.riyaz22@gmail.com>
2023-10-27 11:29:40 +0200

    selector and dispatch

--- shop/src/components/PhoneComponent.js
+++ shop/src/components/PhoneComponent.js
@@ -1,32 +1,22 @@
 import React from "react";
 import phone from "../images/phone.png";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { buyPhone } from "../redux/phone/actionPhone";
 
-function PhoneComponent(props) {
-  console.log(props);
+function PhoneComponent() {
+  const phones = useSelector((state) => state.phones);
+  const dispatch = useDispatch();
+
   return (
     <div className="container">
       <img src={phone} alt="phone"></img>
       <p>
         Disponibilite:
-        <span id="count">{props.phones}</span>
+        <span id="count">{phones}</span>
       </p>
-      <button onClick={() => props.buyPhone()}>Acheter</button>
+      <button onClick={() => dispatch(buyPhone())}>Acheter</button>
     </div>
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    phones: state.phones,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    buyPhone: () => dispatch(buyPhone()),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(PhoneComponent);
+export default PhoneComponent;
